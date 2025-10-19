f1b3575228193a5ca86f88b8f8b7d6ad5d787a69
Aarya Goriya <aaryagoriya@gmail.com>
2025-10-19 23:23:15 +1300

    .

---
+++ Basics/Datatypes/Datatypes.js
@@ -0,0 +1,15 @@
+// String
+let string = "Hello World!";
+console.log(string);
+
+// Float
+let apple = 9.99;
+console.log(typeof(apple));
+
+// Boolean
+let boolean = true;
+console.log(boolean);
+
+// Integer
+let banana = 999;
+console.log(banana);
